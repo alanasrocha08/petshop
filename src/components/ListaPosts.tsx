71f3e22bf7314dfb94ed51eb688e8185d574c3cd
alanasrocha08 <alanasr02@gmail.com>
2025-08-01 10:59:18 -0300

    Adiciona todos os posts

--- src/components/ListaPosts.tsx
+++ src/components/ListaPosts.tsx
@@ -1,7 +1,6 @@
 import { Post } from "@/types/Post";
 import estilos from "./ListaPosts.module.css";
 
-/* Configurando um tipo para este componente e, neste tipo, definimos a prop posts relacionando ela ao tipo Post criado anteriormente. */
 type ListaPostProps = {
   posts: Post[];
 };
@@ -9,10 +8,12 @@ type ListaPostProps = {
 export default function ListaPosts({ posts }: ListaPostProps) {
   return (
     <div className={estilos.posts}>
-      <article>
-        <h3>Título do post</h3>
-        <p>Subtítulo do post...</p>
-      </article>
+      {posts.map((post) => (
+        <article key={post.id}>
+          <h3>{post.titulo}</h3>
+          <p>{post.subtitulo}</p>
+        </article>
+      ))}
     </div>
   );
 }
