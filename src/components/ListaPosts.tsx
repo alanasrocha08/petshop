1aa2201791ddeb240a0b592f2eb8a79589174f09
alanasrocha08 <alanasr02@gmail.com>
2025-08-01 09:51:38 -0300

    Update ListaPosts.tsx

--- src/components/ListaPosts.tsx
+++ src/components/ListaPosts.tsx
@@ -1,6 +1,12 @@
+import { Post } from "@/types/Post";
 import estilos from "./ListaPosts.module.css";
 
-export default function ListaPosts() {
+/* Configurando um tipo para este componente e, neste tipo, definimos a prop posts relacionando ela ao tipo Post criado anteriormente. */
+type ListaPostProps = {
+  posts: Post[];
+};
+
+export default function ListaPosts({ posts }: ListaPostProps) {
   return (
     <div className={estilos.posts}>
       <article>
