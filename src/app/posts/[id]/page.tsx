2b74390ae8e61ba3a068a3438e4dd3dd5897c6a5
alanasrocha08 <alanasr02@gmail.com>
2025-08-04 11:59:47 -0300

    retorno da função DEVE SER um Promise

--- src/app/posts/[id]/page.tsx
+++ src/app/posts/[id]/page.tsx
@@ -6,9 +6,24 @@ type DetalhePostProps = {
   params: Promise<{ id: string }>;
 };
 
+/* A função abaixo precisa: 
+- Receber o id 
+- Executar o acesso à API usando este id(como string) e retornar o post com os dados
+- O retorno da função DEVE SER um Promise
+- Não se esqueça de chamar/usar esta nova função dentro do genare=ateMetadata e do DetalhePost no lugar do código que você irá remover. */
+async function buscarPostPorId(id: string): Promise<Post> {
+  const resposta = await fetch(`http://localhost:2112/posts/${id}`);
+
+  if (!resposta.ok) {
+    throw new Error("Erro ao buscar post: " + resposta.statusText);
+  }
+
+  const post: Post = await resposta.json();
+  return post;
+}
+
 export async function genereteMetadata({ params }: DetalhePostProps) {
   const { id } = await params;
-  console.log(id);
 
   const resposta = await fetch(`http://localhost:2112/posts/${id}`);
 
@@ -26,8 +41,6 @@ export async function genereteMetadata({ params }: DetalhePostProps) {
 
 export default async function DetalhePost({ params }: DetalhePostProps) {
   const { id } = await params;
-  console.log(id);
-
   const resposta = await fetch(`http://localhost:2112/posts/${id}`);
 
   if (!resposta.ok) {
