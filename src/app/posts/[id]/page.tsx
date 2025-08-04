f19d9af0d99c11ece97e72ce09b52dc7e80d1912
alanasrocha08 <alanasr02@gmail.com>
2025-08-04 11:36:18 -0300

    Gera metadados dinamicamente

--- src/app/posts/[id]/page.tsx
+++ src/app/posts/[id]/page.tsx
@@ -1,15 +1,33 @@
 import Container from "@/components/Container";
 import estilos from "./detalhe-post.module.css";
+import { Post } from "@/types/Post";
 
 type DetalhePostProps = {
   params: Promise<{ id: string }>;
 };
 
+export async function genereteMetadata({ params }: DetalhePostProps) {
+  const { id } = await params;
+  console.log(id);
+
+  const resposta = await fetch(`http://localhost:2112/posts/${id}`);
+
+  if (!resposta.ok) {
+    throw new Error("Erro ao buscar post" + resposta.statusText);
+  }
+
+  const dados = await resposta.json();
+
+  return {
+    title: dados.titulo + "| PetShop",
+    description: dados.descricao,
+  };
+}
+
 export default async function DetalhePost({ params }: DetalhePostProps) {
   const { id } = await params;
   console.log(id);
 
-  /* DESAFIO ! Faça um novo fetch na API usando este ID e mostre no HTML abaixo os dados obtidos! */
   const resposta = await fetch(`http://localhost:2112/posts/${id}`);
 
   if (!resposta.ok) {
