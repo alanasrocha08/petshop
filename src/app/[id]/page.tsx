6e54efe20c5110f9b55ceeb1c4de95e64e08390a
alanasrocha08 <alanasr02@gmail.com>
2025-08-04 09:50:50 -0300

    Adiciona CSS detalhe-post

---
+++ src/app/[id]/page.tsx
@@ -0,0 +1,14 @@
+import Container from "@/components/Container";
+import estilos from "./detalhe-post.module.css";
+
+export default function DetalhePost() {
+  return (
+    <article className={estilos.conteudo}>
+      <h2>Título...</h2>
+      <Container>
+        <h3>Categoria...</h3>
+        <p>Descrição...</p>
+      </Container>
+    </article>
+  );
+}
