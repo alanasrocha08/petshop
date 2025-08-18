4f31e6041b09b365c3a697c1415789ec4145e841
alanasrocha08 <alanasr02@gmail.com>
2025-08-18 08:49:58 -0300

    Cria componente categorias com  estilos
    Ainda sem lógica/programação

--- src/components/ListaPosts.tsx
+++ src/components/ListaPosts.tsx
@@ -1,6 +1,7 @@
 import { Post } from "@/types/Post";
 import estilos from "./ListaPosts.module.css";
 import Link from "next/link";
+import FiltroCategorias from "./FiltroCategorias";
 
 type ListaPostProps = {
   posts: Post[];
@@ -8,15 +9,18 @@ type ListaPostProps = {
 
 export default function ListaPosts({ posts }: ListaPostProps) {
   return (
-    <div className={estilos.posts}>
-      {posts.map(({ id, titulo, subtitulo }) => (
-        <article key={id}>
-          <Link href={`/posts/${id}`}>
-            <h3>{titulo}</h3>
-            <p>{subtitulo}</p>
-          </Link>
-        </article>
-      ))}
-    </div>
+    <>
+      <FiltroCategorias />
+      <div className={estilos.posts}>
+        {posts.map(({ id, titulo, subtitulo }) => (
+          <article key={id}>
+            <Link href={`/posts/${id}`}>
+              <h3>{titulo}</h3>
+              <p>{subtitulo}</p>
+            </Link>
+          </article>
+        ))}
+      </div>
+    </>
   );
 }
