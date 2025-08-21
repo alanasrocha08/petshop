af6a8be821c38fd3ba703795d2f5c15eea169d44
alanasrocha08 <alanasr02@gmail.com>
2025-08-21 11:06:28 -0300

    .

--- src/components/Formulario.tsx
+++ src/components/Formulario.tsx
@@ -1,14 +1,63 @@
+"use client";
+
+import { useState } from "react";
 import estilos from "./Formulario.module.css";
+import { enviarContato } from "@/lib/enviar-contato";
+import { useFormStatus } from "react-dom";
+
+function BotaoEnviar() {
+  const { pending } = useFormStatus();
+  return (
+    <button type="submit" disabled={pending}>
+      {pending ? "Processando..," : "Enviar"}
+    </button>
+  );
+}
 
 export default function Formulario() {
+  // Estados para os textos da mensagem
+  const [mensagem, setMensagem] = useState("");
+
+  // Estador para o tipo de mensagem: sucesso ou erro
+  const [tipoMensagem, setTipoMensagem] = useState<"sucesso" | "erro" | "">("");
+
+  async function processarDados(dadosForm: FormData) {
+    //Reset dos states voltando ao valor padrão
+    setMensagem("");
+    setTipoMensagem("");
+
+    try {
+      await enviarContato(dadosForm);
+      setMensagem("Mensagem enviado cim sucesso!");
+      setTipoMensagem("sucesso");
+
+      // Reseta os campos do formulário
+      dadosForm.set("nome", "");
+      dadosForm.set("email", "");
+      dadosForm.set("mensagem", "");
+    } catch (error: unknown) {
+      setMensagem(error instanceof Error ? error.message : "Erro ao enviar");
+      setTipoMensagem("erro");
+    }
+  }
+
   return (
-    <form action="" className={estilos.formulario}>
+    <form action={processarDados} className={estilos.formulario}>
       <div className={estilos.campo}>
-        <label htmlFor="">Nome</label>
+        <label htmlFor="">E-mail</label>
         <input
           type="text"
           name="nome"
           id="nome"
+          placeholder="Escreva seu nome"
+        />
+      </div>
+      <div className={estilos.campo}>
+        <label htmlFor="">Nome</label>
+        <input
+          type="email"
+          name="email"
+          id="email"
           placeholder="Informe um e-mail válido"
         />
       </div>
@@ -22,9 +71,7 @@ export default function Formulario() {
         ></textarea>
       </div>
       <div className={estilos.campo}>
-        <button type="submit" className={estilos.botao}>
-          Enviar
-        </button>
+        <BotaoEnviar />
       </div>
     </form>
   );
